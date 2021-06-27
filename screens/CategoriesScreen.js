99892a0345ecfc811d497a46d635343cc765b8ad
ajayg415 <ajay.g415@gmail.com>
2021-06-27 22:29:50 +0530

    added dummy data to Category Screen

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,17 +1,24 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = ({ navigation }) => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button
-        title="Go To Meals!"
-        onPress={() => {
-          navigation.navigate({ routeName: "CategoryMeals" });
-        }}
-      />
-    </View>
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      numColumns={2}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+    />
   );
 };
 
@@ -21,6 +28,11 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
   },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 100,
+  }
 });
 
 export default CategoriesScreen;
